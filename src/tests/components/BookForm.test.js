57dc6ff4700f30eff6ee66630fd26ba59e95cadd
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-20 15:40:58 -0400

    completed test spies

--- src/tests/components/BookForm.test.js
+++ src/tests/components/BookForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import BookForm from '../../components/BookForm';
 import books from '../fixtures/books';
+import moment from 'moment';
 
 test('should render BookForm Correctly', () => {
   const wrapper = shallow(<BookForm />);
@@ -48,4 +49,37 @@ test('should not set price on input change for invalid price input', () => {
   const wrapper = shallow(<BookForm />);
   wrapper.find('input').at(1).simulate('change', { target: { value } });
   expect(wrapper.state('price')).toBe('');
+});
+
+// https://jestjs.io/docs/en/expect.html
+test('should call onSubmit prop for valid form submission', () => {
+  // jest.fn() returns the new spy function
+  const onSubmitSpy = jest.fn();
+  // render BookForm
+  const wrapper = shallow(<BookForm book={books[1]} onSubmit={onSubmitSpy} />);
+  wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+  expect(wrapper.state('error')).toBe('');
+  // validating that onSubmit function inside BookForm is called with the correct params
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    title: books[1].title,
+    description: books[1].description,
+    price: books[1].price,
+    publishedAt: books[1].publishedAt,
+  });
+});
+
+test('should set createdAt state onDateChange on SingleDatePicker', () => {
+  const now = moment();
+  const wrapper = shallow(<BookForm />);
+  // how to trigger the prop from the child component is done below
+  wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+  expect(wrapper.state('createdAt')).toEqual(now);
+});
+
+test('should set calendarFocused onFocusChange of SingleDatePicker', () => {
+  const focused = true;
+  const wrapper = shallow(<BookForm />);
+  // how to trigger the prop from the child component is done below
+  wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused });
+  expect(wrapper.state('calendarFocused')).toEqual(focused);
 });
