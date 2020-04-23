943bfd6c6fd91d62d263393587848d0df7baa7c7
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-23 09:05:10 -0400

    updated firebase version and commented the test cases in books actions

--- src/tests/actions/books.test.js
+++ src/tests/actions/books.test.js
@@ -13,11 +13,11 @@ import {
 import books from '../fixtures/books';
 import database from '../../firebase/firebase';
 
-// creating the configuration so that all test cases can use the same mock store
+// // creating the configuration so that all test cases can use the same mock store
 const createMockStore = configureMockStore([thunk]);
 var originalTimeout;
 
-beforeEach(() => {
+beforeEach((done) => {
   originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
   const booksData = {};
@@ -28,15 +28,14 @@ beforeEach(() => {
     .ref('books')
     .set(booksData)
     .then(() => {
-      console.log('helloo');
-      // done();
-    });
-  // .catch(done);
+      done();
+    })
+    .catch(done);
 });
 
-afterEach(function () {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-});
+// afterEach(function () {
+//   jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+// });
 
 test('should setup removeBook Action Object', () => {
   const action = removeBook({ id: '123' });
@@ -44,9 +43,10 @@ test('should setup removeBook Action Object', () => {
 });
 
 // testing startRemoveBook Async action
-test('should remove book from firebase', () => {
+test('should remove book from firebase', (done) => {
   const store = createMockStore({});
   const id = books[2].id;
+
   store
     .dispatch(startRemoveBook({ id }))
     .then(() => {
@@ -58,140 +58,150 @@ test('should remove book from firebase', () => {
       return database.ref(`books/${id}`).once('value');
     })
     .then((snapshot) => {
-      expect(snapshot.val()).toBeFalsy();
+      console.log('completed removeBook Async Action', snapshot.val());
+      expect(snapshot.val()).toBe(1);
       // done();
     });
-  // .catch(done)
+  // .catch(done);
 });
 
-test('should setup editBook Action Object', () => {
-  const book = { title: 'React' };
-  const id = '123';
-  const action = editBook(id, book);
-  expect(action).toEqual({ type: 'EDIT_BOOK', id, updates: { ...book } });
-});
+// test('should setup editBook Action Object', () => {
+//   const book = { title: 'React' };
+//   const id = '123';
+//   const action = editBook(id, book);
+//   expect(action).toEqual({ type: 'EDIT_BOOK', id, updates: { ...book } });
+// });
 
-test('should edit book in firebase', () => {
-  const store = createMockStore({});
-  const id = books[1].id;
-  const updates = { price: 541000 };
-  store
-    .dispatch(startEditBook(id, updates))
-    .then(() => {
-      const actions = store.getActions();
-      expect(actions[0]).toEqual({ type: 'EDIT_BOOK', id, updates });
-      return database.ref(`books/${id}`).once('value');
-    })
-    .then((snapshot) => {
-      expect(snapshot.val().price).toBe(updates.price);
-      // done();
-    });
-  //.catch(done);
-});
-// commented this one because this will not work anymore as we have changed the action after adding thunk
-// test('should setup addBook Action Object with default values', () => {
-//   expect(addBook({})).toEqual({
+// test('should edit book in firebase', () => {
+//   const store = createMockStore({});
+//   const id = books[1].id;
+//   const updates = { price: 541000 };
+//   store
+//     .dispatch(startEditBook(id, updates))
+//     .then(() => {
+//       const actions = store.getActions();
+//       expect(actions[0]).toEqual({ type: 'EDIT_BOOK', id, updates });
+//       return database.ref(`books/${id}`).once('value');
+//     })
+//     .then((snapshot) => {
+//       // expect(snapshot.val().price).toBe(updates.price);
+//       // done();
+//     });
+//   //.catch(done);
+// });
+// // commented this one because this will not work anymore as we have changed the action after adding thunk
+// // test('should setup addBook Action Object with default values', () => {
+// //   expect(addBook({})).toEqual({
+// //     type: 'ADD_BOOK',
+// //     book: {
+// //       id: expect.any(String),
+// //       title: '',
+// //       description: '',
+// //       price: 0,
+// //       publishedAt: 0,
+// //     },
+// //   });
+// // });
+
+// test('should setup addBook Action Object with provided values', () => {
+//   // const book = {
+//   //   title: 'React',
+//   //   description: 'React Book Desc',
+//   //   price: 240,
+//   //   publishedAt: 150,
+//   // };
+//   const action = addBook(books[3]);
+//   expect(action).toEqual({
 //     type: 'ADD_BOOK',
-//     book: {
-//       id: expect.any(String),
-//       title: '',
-//       description: '',
-//       price: 0,
-//       publishedAt: 0,
-//     },
+//     // commented as part of thunk
+//     // book: {
+//     //   id: expect.any(String),
+//     //   ...book,
+//     // },
+//     book: books[3],
 //   });
 // });
 
-test('should setup addBook Action Object with provided values', () => {
-  // const book = {
-  //   title: 'React',
-  //   description: 'React Book Desc',
-  //   price: 240,
-  //   publishedAt: 150,
-  // };
-  const action = addBook(books[3]);
-  expect(action).toEqual({
-    type: 'ADD_BOOK',
-    // commented as part of thunk
-    // book: {
-    //   id: expect.any(String),
-    //   ...book,
-    // },
-    book: books[3],
-  });
-});
+// test('should add book to the database and store', (done) => {
+//   const store = createMockStore({});
+//   // we can use the store variable above to dispatch asynchronous redux actions
+//   const book = {
+//     title: 'React Book',
+//     description: 'This is the first react book',
+//     price: 54500,
+//     publishedAt: 0,
+//   };
+//   store
+//     .dispatch(startAddBook(book))
+//     .then(() => {
+//       const actions = store.getActions();
+//       expect(actions[0]).toEqual({
+//         type: 'ADD_BOOK',
+//         book: {
+//           id: expect.any(String),
+//           ...book,
+//         },
+//       });
+//       return database.ref(`books/${actions[0].book.id}`).once('value');
+//     })
+//     .then((snapshot) => {
+//       console.log('################inside');
+//       expect(1).toBe(2);
+//       // expect(snapshot.val()).toEqual(book);
+//       done();
+//     })
+//     .catch(done);
+// });
 
-test('should add book to the database and store', () => {
-  const store = createMockStore({});
-  // we can use the store variable above to dispatch asynchronous redux actions
-  const book = {
-    title: 'React Book',
-    description: 'This is the first react book',
-    price: 54500,
-    publishedAt: 0,
-  };
-  store
-    .dispatch(startAddBook(book))
-    .then(() => {
-      const actions = store.getActions();
-      expect(actions[0]).toEqual({
-        type: 'ADD_BOOK',
-        book: {
-          id: expect.any(String),
-          ...book,
-        },
-      });
-      return database.ref(`books/$actions[0].book.id`).once('value');
-    })
-    .then((snapshot) => {
-      expect(snapshot.val()).toEqual(book);
-    });
-});
+// test('should add book with defaults to database and store', async () => {
+//   const store = createMockStore({});
+//   // we can use the store variable above to dispatch asynchronous redux actions
+//   const bookDefaults = {
+//     title: '',
+//     description: '',
+//     price: 0,
+//     publishedAt: 0,
+//   };
+//   try {
+//     store
+//       .dispatch(startAddBook({}))
+//       .then(() => {
+//         const actions = store.getActions();
+//         expect(actions[0]).toEqual({
+//           type: 'ADD_BOOK',
+//           book: {
+//             id: expect.any(String),
+//             ...bookDefaults,
+//           },
+//         });
+//         return database.ref(`books/${actions[0].book.id}`).once('value');
+//       })
+//       .then((snapshot) => {
+//         expect(1).toBe(2);
+//         expect(snapshot.val()).toEqual(bookDefaults);
+//         // done();
+//       });
+//     // .catch(done);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
-test('should add book with defaults to database and store', () => {
-  const store = createMockStore({});
-  // we can use the store variable above to dispatch asynchronous redux actions
-  const bookDefaults = {
-    title: '',
-    description: '',
-    price: 0,
-    publishedAt: 0,
-  };
-  store
-    .dispatch(startAddBook({}))
-    .then(() => {
-      const actions = store.getActions();
-      expect(actions[0]).toEqual({
-        type: 'ADD_BOOK',
-        book: {
-          id: expect.any(String),
-          ...bookDefaults,
-        },
-      });
-      return database.ref(`books/$actions[0].book.id`).once('value');
-    })
-    .then((snapshot) => {
-      expect(snapshot.val()).toEqual(bookDefaults);
-      // done();
-    });
-  // .catch(done);
-});
+// test('should setup book action object with data', () => {
+//   const action = setBooks(books);
+//   expect(action).toEqual({ type: 'SET_BOOKS', books });
+// });
 
-test('should setup book action object with data', () => {
-  const action = setBooks(books);
-  expect(action).toEqual({ type: 'SET_BOOKS', books });
-});
-
-test('should fetch the books from firebase', () => {
-  // below line creates the store and empty object means that we do not need anything in the store
-  const store = createMockStore({});
-  store.dispatch(startSetBooks()).then(() => {
-    const actions = store.getActions();
-    expect(actions[0]).toEqual({
-      type: 'SET_BOOKS',
-      books,
-    });
-    // done();
-  });
-  // .catch(done);
-});
+// test('should fetch the books from firebase', () => {
+//   // below line creates the store and empty object means that we do not need anything in the store
+//   const store = createMockStore({});
+//   store.dispatch(startSetBooks()).then(() => {
+//     const actions = store.getActions();
+//     expect(actions[0]).toEqual({
+//       type: 'SET_BOOKS',
+//       books,
+//     });
+//     // done();
+//   });
+//   // .catch(done);
+// });
