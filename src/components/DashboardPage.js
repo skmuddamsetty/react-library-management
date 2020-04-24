3958855da96347d4e5a31a551f81eb164c99b312
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-24 01:04:35 -0400

    added sidebar to the results and changed the color to blue for the headers

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -2,11 +2,19 @@ import React from 'react';
 import BooksList from './BooksList';
 import BookListFilters from './BookListFilters';
 import BooksSummary from './BooksSummary';
+import SideBar from './SideBar';
 const LibraryDashboardPage = () => (
   <div>
     <BooksSummary />
     <BookListFilters />
-    <BooksList />
+    <div className='content-container'>
+      <div className='results-layout'>
+        <SideBar />
+        <div className='books-list'>
+          <BooksList />
+        </div>
+      </div>
+    </div>
   </div>
 );
 export default LibraryDashboardPage;
---
+++ src/components/SideBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const SideBar = (props) => (
+  <div className='sidebar'>
+    <div>SideBar</div>
+  </div>
+);
+export default SideBar;
