130ca7b9c667a7ee076de899cc8b11471f412112
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-25 19:33:47 -0400

    added header file and styles for the header with svgs

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
+import MainHeader from './MainHeader';
 
 export const LoginPage = (props) => {
   return (
-    <div className='box-layout'>
-      <div className='box-layout__box'>
-        <h1 className='box-layout__title'>Sunshine Library</h1>
-        <p>Login to manage, checkout and to do much more</p>
-        <button className='button' onClick={props.startLogin}>
-          Login
-        </button>
+    <div>
+      <MainHeader />
+      <div className='box-layout'>
+        <div className='box-layout__box'>
+          <h1 className='box-layout__title'>Sunshine Library</h1>
+          <p>Login to manage, checkout and to do much more</p>
+          <button className='button' onClick={props.startLogin}>
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/MainHeader.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const MainHeader = () => {
+  return (
+    <header className='main-header'>
+      <img src='/images/favicon.png' alt='Logo' className='logo' />
+      <form className='search'>
+        <input
+          type='text'
+          className='search__input'
+          placeholder='Search Books'
+        />
+        <button className='search__button'>
+          <svg className='search__icon'>
+            <use xlinkHref='/images/sprite.svg#icon-magnifying-glass'></use>
+          </svg>
+        </button>
+      </form>
+      <nav className='user-nav'>
+        <div className='user-nav__icon-box'>
+          <svg className='user-nav__icon'>
+            <use xlinkHref='/images/sprite.svg#icon-bookmark'></use>
+          </svg>
+          <span className='user-nav__notification'>0</span>
+        </div>
+        <div className='user-nav__user'>
+          <img
+            src='/images/loader.gif'
+            alt='User Photo'
+            className='user-nav__user-photo'
+          />
+          <span className='user-nav__user-name'>John Doe</span>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
